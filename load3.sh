a630f51527797d6015c2dea7a772b1ac2b8d0dbc
endomorphosis <starworks5@gmail.com>
2024-10-05 00:19:20 -0700

    Refactor load2.sh to update dataset columns

--- load3.sh
+++ load3.sh
@@ -1,4 +1,4 @@
 curl 127.0.0.1:9999/load \
     -X POST \
-    -d '{"dataset":"laion/German-ConcatX-Abstract", "knn_index":"laion/German-ConcatX-M3", "dataset_split": "train", "knn_index_split": "train", "columns": "Concat Abstract"}' \
+    -d '{"dataset":"laion/German-ConcatX-Abstract", "knn_index":"laion/German-ConcatX-M3", "dataset_split": "train", "knn_index_split": "train", "columns": "columns": ["Concat Abstract","Title","URL"] }' \
     -H 'Content-Type: application/json'
