ed857015c55abc9f261cb39148b6429680a407bd
endomorphosis <starworks5@gmail.com>
2024-10-21 21:12:20 -0700

    Refactor code to use requests library for making HTTP requests

--- test/test openvino.py
+++ test/test openvino.py
@@ -1,5 +1,3 @@
-# import numpy as np
-# from ovmsclient import make_grpc_client, make_http_client
 from transformers import BertTokenizer
 import os 
 import requests
@@ -14,37 +12,38 @@ text = "What is the capital of France?"
 encoded_input = tokenizer(text, max_length=128, padding='max_length', truncation=True, return_tensors='pt')
 
 # Display
-print("Input IDs:", encoded_input['input_ids'])
-print("Attention Mask:", encoded_input['attention_mask'])
-print("Token Type IDs:", encoded_input['token_type_ids'])
+# print("Input IDs:", encoded_input['input_ids'])
+# print("Attention Mask:", encoded_input['attention_mask'])
+# print("Token Type IDs:", encoded_input['token_type_ids'])
 
 input_ids = encoded_input['input_ids']
 attention_mask = encoded_input['attention_mask']
 token_type_ids = encoded_input['token_type_ids']
 
-curl = """ curl -X POST https://gte-large-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/gte-large/infer    -H "Content-Type: application/json"    -d '{
-         "inputs": [
-           {
-             "name": "attention_mask",
-             "shape": [1, 128],
-             "datatype": "INT64",
-             "data": {attention_mask}  # Replace with your actual attention mask data
-           },
-           {
-             "name": "input_ids",
-             "shape": [1, 128],
-             "datatype": "INT64",
-             "data": {input_ids}  # Replace with your actual input_ids data
-           },
-           {
-             "name": "token_type_ids",
-             "shape": [1, 128],
-             "datatype": "INT64",
-             "data": {token_type_ids}  # Replace with your actual token_type_ids data
-           }
-         ]
-       }'
-       """
-
-results = subprocess.check_output(curl)
-print(results)
+# Request
+url = "https://gte-large-endomorphosis-dev.apps.cluster.intel.sandbox1234.opentlc.com/v2/models/gte-large/infer"
+headers = {"Content-Type": "application/json"}
+data = {
+    "inputs": [
+        {
+            "name": "attention_mask",
+            "shape": [1, 128],
+            "datatype": "INT64",
+            "data": attention_mask.tolist()
+        },
+        {
+            "name": "input_ids",
+            "shape": [1, 128],
+            "datatype": "INT64",
+            "data": input_ids.tolist()
+        },
+        {
+            "name": "token_type_ids",
+            "shape": [1, 128],
+            "datatype": "INT64",
+            "data": token_type_ids.tolist()
+        }
+    ]
+}
+response = requests.post(url, headers=headers, json=data)
+print(response.json())
