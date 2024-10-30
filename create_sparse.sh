26e3e5ab642188e5073be2405cef1c970f4c7a8b
endomorphosis <starworks5@gmail.com>
2024-10-30 16:43:11 -0700

    Update README.md for improved clarity on endpoint usage and commands; add new scripts for sparse indexing and sharding, and enhance curl command descriptions

--- create_sparse.sh
+++ create_sparse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl 127.0.0.1:/create \
+curl 127.0.0.1:/create_sparse \
     -X POST \
     -d '["TeraflopAI/Caselaw_Access_Project", "train", "text", "/storage/teraflopai/tmp", ["thenlper/gte-small", "Alibaba-NLP/gte-large-en-v1.5", "Alibaba-NLP/gte-Qwen2-1.5B-instruct"]]' \
     -H 'Content-Type: application/json'
+
