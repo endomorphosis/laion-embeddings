0b27a22651755a934863145f5e4f3763bbdf45a7
endomorphosis <starworks5@gmail.com>
2024-10-31 15:09:32 -0700

    Fix formatting by adding missing newlines in __init__.py for improved readability

--- ipfs_embeddings_py/__init__.py
+++ ipfs_embeddings_py/__init__.py
@@ -3,22 +3,26 @@ try:
     from ipfs_embeddings import ipfs_embeddings_py
 except:
     from .ipfs_embeddings import ipfs_embeddings_py
+
 try:
     import ipfs_multiformats
     from ipfs_multiformats import ipfs_multiformats_py
 except:
     from .ipfs_multiformats import ipfs_multiformats_py
+
 try:
     import qdrant_kit
     from qdrant_kit import qdrant_kit_py
 except:
     from .qdrant_kit import qdrant_kit_py
 try:
+    
     import ipfs_parquet_to_car
     from ipfs_parquet_to_car import ipfs_parquet_to_car_py
 except:
     from .ipfs_parquet_to_car import ipfs_parquet_to_car_py
     pass
+
 try:
     import node_parser
     from node_parser import *
