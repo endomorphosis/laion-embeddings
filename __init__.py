9ed274a2bee67f41a7bf0b9c03d1f6033259cd9d
endomorphosis <starworks5@gmail.com>
2024-10-30 17:50:55 -0700

    Add test import to __init__.py and update export list for module accessibility

--- __init__.py
+++ __init__.py
@@ -5,3 +5,5 @@ from .sparse_embeddings import sparse_embeddings
 from .storacha_clusters import storacha_clusters
 from .shard_embeddings import shard_embeddings
 from .ipfs_cluster_index import ipfs_cluster_index
+from .test import test
+export = [ipfs_embeddings_py, create_embeddings, search_embeddings, sparse_embeddings, storacha_clusters, shard_embeddings, ipfs_cluster_index, test]
