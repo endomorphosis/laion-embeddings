6c34d29f2c7e221fe4619564e6c021bd5873e880
endomorphosis <starworks5@gmail.com>
2024-10-31 15:07:26 -0700

    Fix formatting by removing trailing newlines in curl commands across multiple scripts

--- create_sparse.sh
+++ create_sparse.sh
@@ -3,4 +3,3 @@ curl 127.0.0.1:/create_sparse \
     -X POST \
     -d '["TeraflopAI/Caselaw_Access_Project", "train", "text", "/storage/teraflopai/tmp", ["thenlper/gte-small", "Alibaba-NLP/gte-large-en-v1.5", "Alibaba-NLP/gte-Qwen2-1.5B-instruct"]]' \
     -H 'Content-Type: application/json'
-
--- load.sh
+++ load.sh
@@ -2,4 +2,3 @@ curl 127.0.0.1:9999/load \
     -X POST \
     -d '{"dataset":"laion/Wikipedia-X-Concat", "knn_index":"laion/Wikipedia-M3", "dataset_split": "enwiki_concat", "knn_index_split": "enwiki_embed", "columns": "Concat Abstract"}' \
     -H 'Content-Type: application/json'
-
